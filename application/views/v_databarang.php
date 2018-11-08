cde792465947ef3759c59d617dff1443e31fca76
NADA\MyBook14 <dhanty12@gmail.com>
2018-11-09 05:50:09 +0700

    perbaiki tampilan tambah barang

--- application/views/v_databarang.php
+++ application/views/v_databarang.php
@@ -4,9 +4,15 @@
                     <div class="container-fluid">
                         <div class="row">
                             <div class="col-md-12">
-                                <div class="overview-wrap">
-                                    <h2 class="title-1">Data Barang</h2>
-                                </div>
+                                <h3 class="title-5 m-b-35">DATA BARANG</h3>
+                                <div class="table-data__tool">
+                                  <div class="table-data__tool-left">
+                                      <div class="rs-select2--light rs-select2--md">
+                                        <button class="au-btn au-btn-icon au-btn--green au-btn--medium" data-toggle="modal" data-target="#largeModal">
+                                        <i class="zmdi zmdi-plus"></i>Tambah</button>
+                                      </div>
+                               
+                            </div>
                             </div>
                         </div>
 						<div class="row">
@@ -60,6 +66,58 @@
                                 </div>
                         </div>
                 </div>
+                <div class="row">
+                            <div class="col-md-12">
+                                <div class="copyright">
+                                    <p>Copyright © 2018 Colorlib. All rights reserved. Template by <a href="https://colorlib.com">Colorlib</a>.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- modal large -->
+            <div class="modal fade" id="largeModal" tabindex="-1" role="dialog" aria-labelledby="largeModalLabel" aria-hidden="true">
+              <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="largeModalLabel">Tambah Data Barang</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+                    <form class="form-horizontal" action="<?php echo base_url('DataBarang/input')?>" method="post" enctype="multipart/form-data" role="form">
+                        <div class="modal-body">
+                                <div class="form-group">
+                                    <label class="col-lg-4 col-sm-2 control-label">Id Barang</label>
+                                    <div class="col-lg-10">
+                                        <input type="text" class="form-control" name="id_barang" value="" readonly>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-lg-4 col-sm-2 control-label">Nama Barang</label>
+                                    <div class="col-lg-10">
+                                        <input type="text" class="form-control" name="nama_barang" placeholder="Tuliskan Nama">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-lg-4 col-sm-2 control-label">Harga Jasa</label>
+                                    <div class="col-lg-10">
+                                      <input type="number" class="form-control" name="harga_jasa" placeholder="Tuliskan Harga"></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                          </form>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                    <button type="button" class="btn btn-primary" type="submit" name="btnTambah">Simpan</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+      <!-- end modal large -->
                 
         
         <!-- Modal Tambah -->
@@ -101,16 +159,7 @@
               </div>
           </div>
 
-                  <div class="row">
-                            <div class="col-md-12">
-                                <div class="copyright">
-                                    <p>Copyright © 2018 Colorlib. All rights reserved. Template by <a href="https://colorlib.com">Colorlib</a>.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                  
 
     <script src="<?php echo base_url().'assets/js/jquery-3.3.1.js'?>" type="text/javascript"></script>
     <script src="<?php echo base_url().'assets/js/bootstrap.js'?>" type="text/javascript"></script>
