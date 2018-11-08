a2dcf14d62b6565e81e58b94468f95b254da9adc
MARDIANAKOM\Mardiana <now101246@gmail.com>
2018-11-08 11:12:22 +0700

    db

--- application/controllers/InputSewa.php
+++ application/controllers/InputSewa.php
@@ -17,9 +17,10 @@ class InputSewa extends CI_Controller {
             'title'=>'Input Sewa',
             'active_inputsewa'=>'active'
         );
+        $kode['kode'] = $this->Model_app->get_notrans();
         $this->load->view('element/css',$data);
         $this->load->view('element/v_header');
-        $this->load->view('v_inputsewa');
+        $this->load->view('v_inputsewa', $kode);
 	}
 
     public function get_autocomplete(){    //membuat dropdown pilihan di search box
@@ -29,10 +30,54 @@ class InputSewa extends CI_Controller {
             foreach ($result as $row)
                 $arr_result[] = array(
                     'label'=> $row->nama_barang,
+                    'stok'=>$row->stok_barang,
+                    'harga'=>$row->harga_sewa,
                     'id_barang' => $row->id_barang
                 );
                 echo json_encode($arr_result);
             }
         }
+    }
+
+    function inputdetail(){
+        // $no_transaksi = $this->input->post('no_transaksi');
+        // $nama_pemesan = $this->input->post('nama_pemesan');
+        // $tgl_ambil = $this->input->post('tgl_ambil');
+        // $jam_ambil = $this->input->post('jam_ambil');
+        // $no_telp = $this->input->post('no_telp');
+        // $alamat_antar = $this->input->post('alamat');
+        $id_pesan = $this->input->post('id_pesan');
+        $stok = $this->input->post('stok_barang');
+        $harga = $this->input->post('harga_sewa');
+        $jumlah = $this->input->post('jumlah_sewa');
+        $total = $harga*$jumlah;
+        $ceklagi = $this->db->query("SELECT * FROM 'detail_sewa' WHERE id_pesan='".$id_pesan."'")->num_rows();
+        if($ceklagi >= 1){
+            $this->db->query("UPDATE `detail_sewa` SET `nama_pemesan`='$nama_pemesan',`tgl_ambil`='$tgl_ambil',`jam_ambil`='$jam_ambil',`alamat_antar`='$alamat_antar',`no_telp`='$no_telp' WHERE id_pesan='$no_transaksi'");
+        }
+        elseif ($ceklagi == 0) {
+                $this->db->query("INSERT INTO `tabel_detail_pemesan`(`id_pesan`, `nama_pemesan`, `tgl_ambil`, `jam_ambil`, `alamat_antar`,`no_telp`) VALUES ('$no_transaksi','$nama_pemesan','$tgl_ambil','$jam_ambil','$alamat_antar','$no_telp')");
+        }
+        $cek = $this->db->query("SELECT * FROM `tabel_detail_pesan` WHERE id_pesan='".$no_transaksi."' AND id_roti='".$id_roti."'")->num_rows();
+        if($cek >= 1){
+                $this->db->query("UPDATE `tabel_detail_pesan` SET `jumlah`=jumlah+'$jumlah',`total`=total+'$total' WHERE id_pesan='$no_transaksi' AND id_roti='$id_roti'");
+                redirect('Pesanan/input');
+            }
+            
+            
+        
+        // elseif ($cek == 0){
+            
+        //         $data = array(
+        //     'id_pesan' => $no_transaksi,
+        //     'id_roti' => $id_roti,
+        //     'harga' => $harga,
+        //     'jumlah' => $jumlah,
+        //     'total' => $total
+        //     );
+                
+        //     $this->Pesanan_model->inputdetail($data,'tabel_detail_pesan');
+        //     redirect('Pesanan/input');
+        // }
     }
 }
--- bintangraya.sql
+++ bintangraya.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 06 Nov 2018 pada 02.46
--- Versi Server: 10.1.16-MariaDB
--- PHP Version: 5.6.24
+-- Generation Time: Nov 08, 2018 at 05:10 AM
+-- Server version: 10.1.26-MariaDB
+-- PHP Version: 7.1.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -23,46 +25,44 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `barang`
+-- Table structure for table `barang`
 --
 
 CREATE TABLE `barang` (
   `id_barang` varchar(5) NOT NULL,
+  `id_kategori` varchar(2) NOT NULL,
   `nama_barang` varchar(25) NOT NULL,
   `harga_jasa` int(11) NOT NULL,
   `harga_sewa` int(11) NOT NULL,
-  `stok_barang` int(11) NOT NULL,
-  `id_kategori` varchar(2) NOT NULL
+  `stok_barang` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `barang`
+-- Dumping data for table `barang`
 --
 
-INSERT INTO `barang` (`id_barang`, `nama_barang`, `harga_jasa`, `harga_sewa`, `stok_barang`, `id_kategori`) VALUES
-('AM001', 'Piring', 200, 1000, 100, 'AM'),
-('PK001', 'Kipas Angin', 500, 2000, 10, 'PK');
+INSERT INTO `barang` (`id_barang`, `id_kategori`, `nama_barang`, `harga_jasa`, `harga_sewa`, `stok_barang`) VALUES
+('MK001', 'AM', 'Piring', 100, 1000, 500),
+('MK002', 'AM', 'Gelas', 100, 1200, 500);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `detail_sewa`
+-- Table structure for table `detail_sewa`
 --
 
 CREATE TABLE `detail_sewa` (
-  `id_sewa` int(11) NOT NULL,
-  `id_hargatenda` varchar(5) NOT NULL,
-  `id_alat` varchar(5) NOT NULL,
-  `id_barang` varchar(5) NOT NULL,
+  `id_sewa` varchar(6) NOT NULL,
+  `id` varchar(6) NOT NULL,
   `jumlah_barang` int(11) NOT NULL,
-  `harga_total` int(11) NOT NULL,
-  `harga_tagihan` int(11) NOT NULL
+  `harga_sewa` int(11) NOT NULL,
+  `harga_total` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `kategori_barang`
+-- Table structure for table `kategori_barang`
 --
 
 CREATE TABLE `kategori_barang` (
@@ -71,7 +71,7 @@ CREATE TABLE `kategori_barang` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `kategori_barang`
+-- Dumping data for table `kategori_barang`
 --
 
 INSERT INTO `kategori_barang` (`id_kategori`, `nama_kategori`) VALUES
@@ -81,33 +81,25 @@ INSERT INTO `kategori_barang` (`id_kategori`, `nama_kategori`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `paket_tenda`
+-- Table structure for table `paket_tenda`
 --
 
 CREATE TABLE `paket_tenda` (
   `id_hargatenda` varchar(5) NOT NULL,
+  `id_tenda` varchar(5) NOT NULL,
   `jenis_tenda` varchar(25) NOT NULL,
   `sewa_tenda` int(11) NOT NULL,
-  `jasa_tenda` int(11) NOT NULL,
-  `id_tenda` varchar(5) NOT NULL
+  `jasa_tenda` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `paket_tenda`
---
-
-INSERT INTO `paket_tenda` (`id_hargatenda`, `jenis_tenda`, `sewa_tenda`, `jasa_tenda`, `id_tenda`) VALUES
-('TNP01', 'Tenda Non Plafon', 125000, 100000, 'TD001'),
-('TNP02', 'Tenda Non Plafon', 125000, 100000, 'TD002');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `pelanggan`
+-- Table structure for table `pelanggan`
 --
 
 CREATE TABLE `pelanggan` (
-  `id_pelanggan` int(11) NOT NULL,
+  `id_pelanggan` int(6) NOT NULL,
   `nama_pelanggan` varchar(15) NOT NULL,
   `alamat_pelanggan` text NOT NULL,
   `telp_pelanggan` varchar(15) NOT NULL
@@ -116,48 +108,39 @@ CREATE TABLE `pelanggan` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `pembayaran`
---
-
-CREATE TABLE `pembayaran` (
-  `id_bayar` int(11) NOT NULL,
-  `id_sewa` int(11) NOT NULL,
-  `DP` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `pengembalian`
+-- Table structure for table `pengembalian`
 --
 
 CREATE TABLE `pengembalian` (
-  `id_kembali` int(11) NOT NULL,
-  `id_sewa` int(11) NOT NULL
+  `id_kembali` varchar(6) NOT NULL,
+  `id_sewa` varchar(6) NOT NULL,
+  `tgl_kembali` date NOT NULL,
+  `jumlah_kembali` int(11) NOT NULL,
+  `hilang/rusak` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `sewa`
+-- Table structure for table `sewa`
 --
 
 CREATE TABLE `sewa` (
-  `id_sewa` int(11) NOT NULL,
+  `id_sewa` varchar(6) NOT NULL,
+  `id_pelanggan` int(11) NOT NULL,
   `tgl_sekarang` date NOT NULL,
   `tgl_pasang` date NOT NULL,
   `tgl_acara` date NOT NULL,
   `tgl_bongkar` date NOT NULL,
-  `id_pelanggan` int(11) NOT NULL,
-  `ids_tenda` varchar(5) NOT NULL,
-  `ids_alat` varchar(5) NOT NULL,
-  `ids_barang` varchar(5) NOT NULL
+  `total_tagihan` int(11) NOT NULL,
+  `dp` int(11) NOT NULL,
+  `pelunasan` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tenda`
+-- Table structure for table `tenda`
 --
 
 CREATE TABLE `tenda` (
@@ -167,7 +150,7 @@ CREATE TABLE `tenda` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `tenda`
+-- Dumping data for table `tenda`
 --
 
 INSERT INTO `tenda` (`id_tenda`, `ukuran_tenda`, `stok_tenda`) VALUES
@@ -177,7 +160,7 @@ INSERT INTO `tenda` (`id_tenda`, `ukuran_tenda`, `stok_tenda`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
@@ -187,7 +170,7 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id_user`, `username`, `password`) VALUES
@@ -202,16 +185,15 @@ INSERT INTO `user` (`id_user`, `username`, `password`) VALUES
 --
 ALTER TABLE `barang`
   ADD PRIMARY KEY (`id_barang`),
-  ADD UNIQUE KEY `id_kategori` (`id_kategori`);
+  ADD KEY `id_kategori` (`id_kategori`),
+  ADD KEY `id_kategori_2` (`id_kategori`);
 
 --
 -- Indexes for table `detail_sewa`
 --
 ALTER TABLE `detail_sewa`
-  ADD UNIQUE KEY `id_sewa` (`id_sewa`),
-  ADD UNIQUE KEY `id_hargatenda` (`id_hargatenda`),
-  ADD UNIQUE KEY `id_alat` (`id_alat`),
-  ADD UNIQUE KEY `id_barang` (`id_barang`);
+  ADD KEY `id_sewa` (`id_sewa`),
+  ADD KEY `id` (`id`);
 
 --
 -- Indexes for table `kategori_barang`
@@ -224,7 +206,7 @@ ALTER TABLE `kategori_barang`
 --
 ALTER TABLE `paket_tenda`
   ADD PRIMARY KEY (`id_hargatenda`),
-  ADD UNIQUE KEY `id_tenda` (`id_tenda`);
+  ADD KEY `id_sewa` (`id_tenda`);
 
 --
 -- Indexes for table `pelanggan`
@@ -233,25 +215,18 @@ ALTER TABLE `pelanggan`
   ADD PRIMARY KEY (`id_pelanggan`);
 
 --
--- Indexes for table `pembayaran`
---
-ALTER TABLE `pembayaran`
-  ADD PRIMARY KEY (`id_bayar`),
-  ADD UNIQUE KEY `id_sewa` (`id_sewa`);
-
---
 -- Indexes for table `pengembalian`
 --
 ALTER TABLE `pengembalian`
   ADD PRIMARY KEY (`id_kembali`),
-  ADD UNIQUE KEY `id_sewa` (`id_sewa`);
+  ADD KEY `id_sewa` (`id_sewa`);
 
 --
 -- Indexes for table `sewa`
 --
 ALTER TABLE `sewa`
   ADD PRIMARY KEY (`id_sewa`),
-  ADD UNIQUE KEY `id_pelanggan` (`id_pelanggan`);
+  ADD KEY `id_pelanggan` (`id_pelanggan`);
 
 --
 -- Indexes for table `tenda`
@@ -273,68 +248,46 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `pelanggan`
 --
 ALTER TABLE `pelanggan`
-  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `pembayaran`
---
-ALTER TABLE `pembayaran`
-  MODIFY `id_bayar` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `pengembalian`
---
-ALTER TABLE `pengembalian`
-  MODIFY `id_kembali` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `sewa`
---
-ALTER TABLE `sewa`
-  MODIFY `id_sewa` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_pelanggan` int(6) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+-- Constraints for dumped tables
 --
 
 --
--- Ketidakleluasaan untuk tabel `barang`
+-- Constraints for table `barang`
 --
 ALTER TABLE `barang`
   ADD CONSTRAINT `barang_ibfk_1` FOREIGN KEY (`id_kategori`) REFERENCES `kategori_barang` (`id_kategori`);
 
 --
--- Ketidakleluasaan untuk tabel `detail_sewa`
+-- Constraints for table `detail_sewa`
 --
 ALTER TABLE `detail_sewa`
-  ADD CONSTRAINT `detail_sewa_ibfk_1` FOREIGN KEY (`id_sewa`) REFERENCES `sewa` (`id_sewa`),
-  ADD CONSTRAINT `detail_sewa_ibfk_2` FOREIGN KEY (`id_hargatenda`) REFERENCES `paket_tenda` (`id_hargatenda`),
-  ADD CONSTRAINT `detail_sewa_ibfk_3` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id_barang`);
+  ADD CONSTRAINT `detail_sewa_ibfk_1` FOREIGN KEY (`id_sewa`) REFERENCES `sewa` (`id_sewa`);
 
 --
--- Ketidakleluasaan untuk tabel `paket_tenda`
+-- Constraints for table `paket_tenda`
 --
 ALTER TABLE `paket_tenda`
   ADD CONSTRAINT `paket_tenda_ibfk_1` FOREIGN KEY (`id_tenda`) REFERENCES `tenda` (`id_tenda`);
 
 --
--- Ketidakleluasaan untuk tabel `pembayaran`
---
-ALTER TABLE `pembayaran`
-  ADD CONSTRAINT `pembayaran_ibfk_1` FOREIGN KEY (`id_sewa`) REFERENCES `sewa` (`id_sewa`);
-
---
--- Ketidakleluasaan untuk tabel `pengembalian`
+-- Constraints for table `pengembalian`
 --
 ALTER TABLE `pengembalian`
   ADD CONSTRAINT `pengembalian_ibfk_1` FOREIGN KEY (`id_sewa`) REFERENCES `sewa` (`id_sewa`);
 
 --
--- Ketidakleluasaan untuk tabel `sewa`
+-- Constraints for table `sewa`
 --
 ALTER TABLE `sewa`
   ADD CONSTRAINT `sewa_ibfk_1` FOREIGN KEY (`id_pelanggan`) REFERENCES `pelanggan` (`id_pelanggan`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
