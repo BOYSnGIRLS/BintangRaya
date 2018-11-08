ce07b99a8bb3b2d54de03795942d71359a6faea6
NADA\MyBook14 <dhanty12@gmail.com>
2018-11-08 11:48:43 +0700

    tampil kategori

--- application/controllers/DataBarang.php
+++ application/controllers/DataBarang.php
@@ -26,9 +26,22 @@ class DataBarang extends CI_Controller {
 
     function home(){
         $data=array(
-            'title'=>'Data Barang',
+            'title'=>'Data Tenda',
             'active_dashboard'=>'active',
             'data'=>$this->Model_Barang->get_data()
+        );
+
+        $this->load->view('element/css',$data);
+        $this->load->view('element/v_header');
+        $this->load->view('v_datatenda',$data);    
+        $this->load->view('element/v_footer');
+    }
+
+     function home2(){
+        $data=array(
+            'title'=>'Data Barang',
+            'active_dashboard'=>'active',
+            'data'=>$this->Model_Barang->get_data2()
         );
 
         $this->load->view('element/css',$data);
@@ -50,19 +63,7 @@ class DataBarang extends CI_Controller {
         $this->load->view('element/v_footer');
     }
 
-    function home2(){
-        $data=array(
-            'title'=>'Data Barang',
-            'active_dashboard'=>'active',
-            'data'=>$this->Model_Barang->get_data2()
-        );
-
-        $this->load->view('element/css',$data);
-        $this->load->view('element/v_header');
-        $this->load->view('v_databarang',$data);    
-        $this->load->view('element/v_footer');
-    }
-
+   
     function input(){
         if (isset($_POST['btnTambah'])){
             $data = $this->Model_Barang->input(array (
@@ -87,6 +88,16 @@ class DataBarang extends CI_Controller {
     function delete($id){
         $this->Model_Barang->delete($id);
         redirect('DataBarang/home');
+    }
+
+    function delete2($id){
+        $this->Model_Barang->delete($id);
+        redirect('DataBarang/home2');
+    }
+
+     function delete3($id){
+        $this->Model_Barang->delete($id);
+        redirect('DataBarang/home3');
     }
 
     function edit(){
--- application/views/v_datatenda.php
+++ application/views/v_datatenda.php
@@ -5,7 +5,7 @@
                         <div class="row">
                             <div class="col-md-12">
                                 <div class="overview-wrap">
-                                    <h2 class="title-1">Data Barang</h2>
+                                    <h2 class="title-1">Data Tenda</h2>
                                 </div>
                             </div>
                         </div>
@@ -13,7 +13,7 @@
                             <div class="col-md-12">
                                 <div class="overview-wrap">
                                     
-									<h3 class="title-1"><a href="<?php echo base_url()?>DataBarang/input">Tambah Barang</a></h3>
+									<h3 class="title-1"><a href="<?php echo base_url()?>DataBarang/input">Tambah Tenda</a></h3>
                                 </div>
                             </div>
                         </div>
@@ -22,9 +22,10 @@
                                         <thead>
                                             <tr>
                                                 <th>No</th>
-                                                <th>Kode Barang</th>
-                                                <th>Nama Barang</th>
-												<th>Stok Barang</th>
+                                                <th>Kode Tenda</th>
+                                                <th>Jenis Tenda</th>
+                                                <th>Ukuran Tenda</th>
+												<th>Stok Tenda</th>
 												<th>Harga Sewa</th>
 												<th>Harga Jasa</th>
 												<th colspan="2">Aksi</th>
@@ -36,17 +37,18 @@
                                             foreach ($data as $row): ?>
                                             <tr>
                                                 <td><?php echo $no;?></td>
-                                                <td><?php echo $row->id_barang;?></td>
-                                                <td><?php echo $row->nama_barang;?></td>
-                                                <td><?php echo $row->stok_barang;?></td>
-                                                <td><?php echo $row->harga_sewa;?></td>
-                                                 <td><?php echo $row->harga_jasa;?></td>
+                                                <td><?php echo $row->id_hargatenda;?></td>
+                                                <td><?php echo $row->jenis_tenda;?></td>
+                                                <td><?php echo $row->ukuran_tenda;?></td>
+                                                <td><?php echo $row->stok_tenda;?></td>
+                                                <td><?php echo $row->sewa_tenda;?></td>
+                                                 <td><?php echo $row->jasa_tenda;?></td>
                                                  <!-- <td>EDIT</td> -->
                                                  <!-- <td>HAPUS</td> -->
 
-                                                <td><a href="<?php echo base_url(); ?>DataBarang/edit/<?php echo $row->id_barang;?>">Edit</a></td>
+                                                <td><a href="<?php echo base_url(); ?>DataBarang/edit/<?php echo $row->id_hargatenda;?>">Edit</a></td>
 
-                                                <td><a href="<?php echo base_url(); ?>DataBarang/delete2/<?php echo $row->id_barang;?>">Hapus</a></td>
+                                                <td><a href="<?php echo base_url(); ?>DataBarang/delete/<?php echo $row->id_hargatenda;?>">Hapus</a></td>
                                             </tr>
                                             <?php $no++;
                                             endforeach;?>
