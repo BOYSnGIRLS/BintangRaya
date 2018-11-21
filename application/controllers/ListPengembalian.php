63107900a312cc72cf010169fdf0f6f3d314f717
MARDIANAKOM\Mardiana <now101246@gmail.com>
2018-11-21 21:11:51 +0700

    pengembalian sama tgl transaksi

--- application/controllers/ListPengembalian.php
+++ application/controllers/ListPengembalian.php
@@ -4,7 +4,7 @@ class ListPengembalian extends CI_Controller {
 
 	function __construct(){
         parent::__construct();
-        $this->load->model('Model_app');
+        $this->load->model('Model_Pengembalian');
 
         $this->load->library('session');
         $this->load->helper('url');
@@ -16,15 +16,31 @@ class ListPengembalian extends CI_Controller {
 
 	public function index(){	
         if($this->session->userdata('username')){
-		$data=array(
-            'title'=>'List Pengembalian'
-        );
-        $this->load->view('element/css',$data);
+    		$data=array(
+                'title'=>'List Pengembalian',
+                'data' => $this->Model_Pengembalian->tampil_semua()
+            );
+            $this->load->view('element/css',$data);
+            $this->load->view('element/v_header');
+            $this->load->view('v_listpengembalian', $data);
+            $this->load->view('element/v_footer');
+    	   }else{
+            redirect('Login');
+           }
+        }
+
+    public function detail(){
+        $id = $this->uri->segment(3);
+        $title['title']='List Pengembalian';
+        $data=array(
+                'data' => $this->Model_Pengembalian->tampil($id),
+                'detail_sewa1' => $this->Model_Pengembalian->get_sewa1($id),
+                'detail_sewa2' =>$this->Model_Pengembalian->get_sewa2($id)
+            );
+        $this->load->view('element/css',$title);
         $this->load->view('element/v_header');
-        $this->load->view('v_listpengembalian');
+        $this->load->view('v_detailkembali', $data);
         $this->load->view('element/v_footer');
-	   }else{
-        redirect('Login');
-       }
+
     }
 }
